31d96fc985ef1d2c659e8d14dada6fbaafc60996
gianmarco.bellusci <g.bellusci1@campus.unimib.it>
2022-11-10 09:03:37 +0100

    primo commit della giornata

--- src/app/home-admin/home-admin.component.ts
+++ src/app/home-admin/home-admin.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
+import {RestApiService} from "../services/rest-api.service";
 
 
 @Component({
@@ -9,13 +10,35 @@ import {ActivatedRoute, Router} from "@angular/router";
 })
 export class HomeAdminComponent implements OnInit {
 
-  constructor(private router: Router,private route:ActivatedRoute) { }
+  public mail: string="";
+
+  constructor(private router: Router, private ras: RestApiService, private route: ActivatedRoute) { }
+  public error:           string  = "";
+  public response: any;
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(params=>{
+      this.mail = params["mail"];
+    });
+
+    this.getAllSurveys();
   }
 
-  logout(){
-    this.router.navigateByUrl("/home")
+
+  public async getAllSurveys() {
+    this.error = "";
+
+    await this.ras.callApi('http://localhost:8080/surveySpringBoot/api/surveys', 'GET',null)
+      .then((res) => {
+        console.log(res);
+        this.response = res;
+      }).catch((err) => {
+        this.error = "Something went WRONG!!";
+      });
+  }
+
+  logout() {
+    this.router.navigateByUrl('');
   }
 
 }
