fd270dc89f51fde154e51137748fa59047fb8f33
gabriele.galizzi <gabriele.galizzi@gmail.com>
2022-11-10 14:26:27 +0100

    take-survey

--- src/app/take-survey/take-survey.component.ts
+++ src/app/take-survey/take-survey.component.ts
@@ -11,17 +11,35 @@ export class TakeSurveyComponent implements OnInit {
   public description: string="";
   public id: any;
   public name: string="";
+  public _id: any;
+  private error: string="";
+  private response: any;
 
   constructor(private ras: RestApiService,private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.route.queryParams.subscribe(params=>{
+      this.route.queryParams.subscribe(params=>{
 
       this.description = params["description"];
       this.id = params["id"];
       this.name = params["name"];
 
+      this.readSurvey(this.id);
+
     })
+  }
+
+  public async readSurvey(id: any){
+    this._id = id;
+    this.error = "";
+    await this.ras.callApi('http://localhost:8080/surveySpringBoot/api/questions', 'GET',this._id)
+      .then((res) => {
+        console.log(res);
+        this.response = res;
+      }).catch((err) => {
+        this.error = "Something went WRONG!!";
+        console.log(this.error);
+      });
   }
 
 }
